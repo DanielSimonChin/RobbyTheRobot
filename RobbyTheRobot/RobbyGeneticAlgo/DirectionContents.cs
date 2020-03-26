9f0011a231119aab95fd5a2b85f4632f303fd364
ObiedUllah <obiedullah@outlook.com>
2020-03-26 09:14:40 -0400

    completed content and directioncontent

---
+++ RobbyTheRobot/RobbyGeneticAlgo/DirectionContents.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobbyGeneticAlgo
+{
+    public struct DirectionContents
+    {
+        Contents N { get; set; }
+        Contents S { get; set; }
+        Contents E { get; set; }
+        Contents W { get; set; }
+        Contents current { get; set; }
+    }
+}
